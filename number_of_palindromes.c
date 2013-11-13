cafa6e4f2e00300d95e6524505c7b29dcb836eb3
Mingming Wang <mingming.wang@innocellence.com>
2013-11-13 10:38:11 +0800

    Solution v1 for Number of Palindromes

--- number_of_palindromes.c
+++ number_of_palindromes.c
@@ -3,24 +3,33 @@
 // For example the input is 'abba' then the possible 
 // palindromes= a, b, b, a, bb, abba 
 // So the result is 6. 
+#include <stdio.h>
 int number_of_palindromes (char *word, int length){
 
     int count = length;
 
     for (int i = 0; i < length; i++){
-        for (int j = i+1; j < length, j++){
+        for (int j = i+1; j < length; j++){
           int left = i;
           int right = j;
-          while(word[left]==word[right]&&left<=right){
+          while(word[left]==word[right]){
 
             left++;
             right--;
+            if (left>=right){
+              count ++;
+              break;
+            }
           }
           
-
-
         }
     }
 
-    return count
+    return count;
+}
+main (){
+
+  char word[] = "ababa";
+  int count = number_of_palindromes(word,5);
+  printf("%d\n", count);
 }
